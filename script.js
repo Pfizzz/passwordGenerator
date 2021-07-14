c83e5b5bdd9ba9273f7688bf80444a234729e9c2
Pfizz <pfiste80@gmail.com>
2021-07-13 23:53:46 -0400

    Final code cleanup before submitting

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
-
-// Assignment code here
 //array of special characters
 const specialChar = ["!@#$%^&*()_+-=[]{},./?"];
 //array of numbers
@@ -24,69 +21,60 @@ var numberConfirm = "";
 var lowerConfirm = "";
 var upperConfirm = "";
 
-
-
-//FUNCTION to generate emptyChar array (must be above generatePassword function due to scope)
-var generateBank = function() {
+// function to generate emptyChar array (must be above generatePassword function due to scope)
+var generateBank = function () {
   passFinal = "";
   var specialConfirm = confirm("Would you like to include special characters?");
   var numberConfirm = confirm("Would you like to include numbers?");
   var lowerConfirm = confirm("Would you like to include lower-case letters?");
   var upperConfirm = confirm("Yould you like to include upper-case letters?");
 
-  //if statement to add characters to empty array
-  if(specialConfirm === true || numberConfirm === true || lowerConfirm === true || upperConfirm === true) {
-    if(specialConfirm === true) {
+  // if statement to add characters to empty array
+  if (specialConfirm === true || numberConfirm === true || lowerConfirm === true || upperConfirm === true) {
+    if (specialConfirm === true) {
       emptyChar = emptyChar.concat(specialChar);
     }
-    if(numberConfirm === true) {
+    if (numberConfirm === true) {
       emptyChar = emptyChar.concat(numberChar);
     }
-    if(lowerConfirm === true) {
+    if (lowerConfirm === true) {
       emptyChar = emptyChar.concat(lowerChar);
     }
-    if(upperConfirm === true) {
+    if (upperConfirm === true) {
       emptyChar = emptyChar.concat(upperChar);
     }
-  
+
   } else {
     alert("You need to choose characters for your password!");
     generateBank();
   }
 }
 
-
-
-// FUNCTION ask the user how many characters
-var generatePassword = function() {
+// function to ask the user how many characters
+var generatePassword = function () {
   let passLength = 0;
-    while (passLength < 8 || passLength > 128 ) {
+  while (passLength < 8 || passLength > 128) {
     //this stores the amount of characters user wants into passLength variable
-      passLength = prompt("Select a password length between between 8 and 128.");
-    }
-    // this if statement checks if the user entered the correct value for the prompt.
-    if(passLength >= 8 || passLength <= 128) {
-      //then sends the user to create the password bank.
-      generateBank();
-      //add the for loop with iterator here
-      for(var i = 0; i < passLength; i++){
-        passFinal += emptyChar[Math.floor(Math.random() * emptyChar.length)]
-      };
-      //returns the final password and sends it back to writePassword().
-      return passFinal;
-      } else {
-        generatePassword();
-      }
+    passLength = prompt("Select a password length between between 8 and 128.");
+  }
+  // this if statement checks if the user entered the correct value for the prompt.
+  if (passLength >= 8 || passLength <= 128) {
+    //then sends the user to create the password bank.
+    generateBank();
+    //add the for loop with iterator here
+    for (var i = 0; i < passLength; i++) {
+      passFinal += emptyChar[Math.floor(Math.random() * emptyChar.length)]
+    };
+    //returns the final password and sends it back to writePassword().
+    return passFinal;
+  } else {
+    generatePassword();
+  }
 }
-  
 
-
-// return the password
-
-  
 // Write password to the #password input
 function writePassword() {
-  
+
   var password = generatePassword();
 
   var passwordText = document.querySelector("#password");
